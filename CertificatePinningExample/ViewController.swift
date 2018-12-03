94cda638c6d89b44637cfe150aab8e540af81bbd
DavidKmn <davidk.9889@gmail.com>
2018-12-03 20:52:51 +0000

    First Commit

---
+++ CertificatePinningExample/ViewController.swift
@@ -0,0 +1,45 @@
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    
+    private var sessionManager: SessionManager?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let url = URL(string: "https://www.apple.com")!
+        enableCertificatePinning()
+        sessionManager?.request(url).responseData { result in
+            if let data = result.data, let requestURL = result.request?.url {
+                self.webView.load(data,
+                                  mimeType: result.response?.mimeType ?? "",
+                                  textEncodingName: result.response?.textEncodingName ?? "",
+                                  baseURL: requestURL)
+            }
+        }
+    }
+    
+    private func enableCertificatePinning() {
+        let certificates: [SecCertificate] = getCertificates()
+        let trustPolicy = ServerTrustPolicy.pinCertificates(
+            certificates: certificates,
+            validateCertificateChain: true,
+            validateHost: true)
+        let trustPolicies = [ "www.apple.com": trustPolicy ]
+        let policyManager =  ServerTrustPolicyManager(policies: trustPolicies)
+        sessionManager = SessionManager(
+            configuration: .default,
+            serverTrustPolicyManager: policyManager)
+    }
+    
+    private func getCertificates() -> [SecCertificate] {
+        let url = Bundle.main.url(forResource: "appleCert", withExtension: "cer")!
+        let localCertificate = try! Data(contentsOf: url) as CFData
+        guard let certificates = SecCertificateCreateWithData(nil, localCertificate) else { return [] }
+        
+        return [certificates]
+    }
+}
